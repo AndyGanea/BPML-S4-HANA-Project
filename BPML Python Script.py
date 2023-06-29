93fab2e90c9005026d4eac9fd00754c8b8ed5ea7
Andy Ganea <43685211+AndyGanea@users.noreply.github.com>
2023-06-29 12:00:12 -0400

    Added Excel Output Functionality + Other Functions

--- BPML Python Script.py
+++ BPML Python Script.py
@@ -1,22 +1,33 @@
 import pandas as pd
 import math
+import openpyxl
+import xlsxwriter
+
+def find_common_element(list1, list2):  # Function that checks if an employee has only 1 role in a certain category
+    common_elements = set(list1) & set(list2)  # Find common elements using set intersection
+    return len(common_elements) == 1 # Returns TRUE if and only if the lists have one element in common
+
+def count_common_elements(list1, list2):
+    common_elements = set(list1) & set(list2)  # Find common elements using set intersection
+    return len(common_elements)
+
 
 excel_file = r"C:\Users\aganea\Documents\Project_Elevate_BPML (3).xlsx"
 sheet_name = "SteriMax User Mapping"
 df = pd.read_excel(excel_file, sheet_name=sheet_name)
 
-for col in df:
+for col in df: # Replace any x in the Excel file with the column's name
     df[col]=df[col].astype(str).apply(lambda x: x.replace('x',df[col].name))
 
 # Define an empty dictionary to store the data
 data_dict = {}
 
-# Iterate over the rows in the DataFrame
+# Iterate over the rows in the Excel File
 for index, row in df.iterrows():
     # Create a dictionary entry for each row
-    key = row[df.columns[1]]
+    key = row[df.columns[1]] # Keys are the names of the employees, read from the second column in Excel
     row_dict = {}
-    for column in df.columns[2:]:
+    for column in df.columns[2:]: # Checks for values in the 3rd column onwards
         value = row[column]
         row_dict[column] = value
     # Add the row dictionary to the data dictionary
@@ -47,9 +58,17 @@ for key, internal_dict in extracted_dicts:
 advanced_user_list = []
 core_user_list = []
 self_service_list = []
+
 advanced_user_constant_list = []
 core_user_constant_list = []
 self_service_constant_list = []
+
+only_one_advanced_user_list = []
+only_one_core_user_list = []
+only_one_self_service_user_list = []
+
+employee_list_with_counts = []
+
 
 sheet_name = "Role Mapping"
 df2 = pd.read_excel(excel_file, sheet_name=sheet_name)
@@ -86,8 +105,175 @@ for dic in employee_list: # Iterates through the master list of dictionaries and
         else:
             self_service_list.append(key)
 
+for dic in employee_list: # Iterates through the master list of dictionaries and sees if someone has one and only one role in a certain category and adds them to another, separate list
+    for key, employee_value in dic.items():
+        advanced_user_result = find_common_element(advanced_user_constant_list, employee_value)
+        core_user_result = find_common_element(core_user_constant_list, employee_value)
+        self_service_user_result = find_common_element(self_service_constant_list, employee_value)
+        if advanced_user_result:
+            only_one_advanced_user_list.append(key)
+        if core_user_result:
+            only_one_core_user_list.append(key)
+        if self_service_user_result:
+            only_one_self_service_user_list.append(key)
+
+for dic in employee_list: # Iterates through the master list of dictionaries and sees how many roles of each type someone has
+    for key, employee_value in dic.items():
+        advanced_user_count = count_common_elements(advanced_user_constant_list, employee_value)
+        core_user_count = count_common_elements(core_user_constant_list, employee_value)
+        self_service_user_count = count_common_elements(self_service_constant_list, employee_value)
+
+        employee_count_dict = {}
+
+        employee_count_dict["Name"] = key
+        employee_count_dict["Advanced User Roles"] = advanced_user_count
+        employee_count_dict["Core User Roles"] = core_user_count
+        employee_count_dict["Self Service Roles"] = self_service_user_count
+
+        employee_list_with_counts.append(employee_count_dict)
+
+
+
 total_FUE = (len(advanced_user_list) * 1) + (len(core_user_list) * 0.2) + (len(self_service_constant_list) * 0.0333) # Calculate Total FUE based on doc rules
 print("The total FUE used by the organization is: " + str(total_FUE))
+# print(only_one_advanced_user_list)
+# print(only_one_core_user_list)
+# print(only_one_self_service_user_list)
+print(employee_list_with_counts)
+
+
+## Exporting Data to Another Excel Sheet
+
+excel_file = r"C:\Users\aganea\Documents\output.xlsx"
+
+# Wipe current contents to prepare for overwrite
+workbook = openpyxl.load_workbook(excel_file)
+sheet = workbook.active
+sheet.delete_rows(1, sheet.max_row)
+workbook.save(excel_file)
+
+# Write Counts for Each Employee
+df = pd.DataFrame(employee_list_with_counts)
+start_row = 0
+start_col = 0
+
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1', index=False, startrow=start_row, startcol=start_col)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df):  # loop through all columns to auto-adjust widths
+        series = df[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+# Write The Lists to the Next Columns
+
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df1 = pd.DataFrame({"Advanced Users": advanced_user_list})
+df1 = pd.concat([existing_data, df1], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df1.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df1):  # loop through all columns to auto-adjust widths
+        series = df1[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df2 = pd.DataFrame({"Core Users": core_user_list})
+df2 = pd.concat([existing_data, df2], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df2.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df2):  # loop through all columns to auto-adjust widths
+        series = df2[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df3 = pd.DataFrame({"Self-Service Users": self_service_list})
+df3 = pd.concat([existing_data, df3], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df3.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df3):  # loop through all columns to auto-adjust widths
+        series = df3[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df4 = pd.DataFrame({"Advanced Users with Only One Role": only_one_advanced_user_list})
+df4 = pd.concat([existing_data, df4], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df4.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df4):  # loop through all columns to auto-adjust widths
+        series = df4[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df5 = pd.DataFrame({"Core Users with Only One Role": only_one_core_user_list})
+df5 = pd.concat([existing_data, df5], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df5.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df5):  # loop through all columns to auto-adjust widths
+        series = df5[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+existing_data = pd.read_excel(excel_file)
+start_col = existing_data.shape[1]
+df6 = pd.DataFrame({"Self-Service Users with Only One Role": only_one_self_service_user_list})
+df6 = pd.concat([existing_data, df6], axis=1)
+writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+df6.to_excel(writer, sheet_name='Sheet1', index=False)
+worksheet = writer.sheets['Sheet1']
+for idx, col in enumerate(df6):  # loop through all columns to auto-adjust widths
+        series = df6[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+            )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+writer.close()
+
+
+print(f"Data exported to '{excel_file}' successfully.")
+
+
+
 
 
 
