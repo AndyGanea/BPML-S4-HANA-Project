26f6d908f7ca9afc2986a96a96aed8677cf2190e
Andy Ganea <43685211+AndyGanea@users.noreply.github.com>
2023-07-14 15:20:43 -0400

    Fixed Powershell

--- Install_Python.ps1
+++ Install_Python.ps1
@@ -12,7 +12,7 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait
 
 # Install pip libraries
-$pipLibraries = 'numpy', 'pandas', 'matplotlib', 'altgraph', 'certifi', 'cffi', 'charset-normalizer', 'cryptography', 'distlib', 'et-xmlfile', 'filelock', 'idna', 'jwt', 'msal', 'openpyxl', 'pefile', 'platformdirs', 'pycparser', 'python-dateutil', 'pytz', 'requests', 'sharepy', 'six, 'tzdata', 'xlrd', 'xlsxwriter', 'xlwt'
+$pipLibraries = 'numpy', 'pandas', 'matplotlib', 'altgraph', 'certifi', 'cffi', 'charset-normalizer', 'cryptography', 'distlib', 'et-xmlfile', 'filelock', 'idna', 'jwt', 'msal', 'openpyxl', 'pefile', 'platformdirs', 'pycparser', 'python-dateutil', 'pytz', 'requests', 'sharepy', 'six', 'tzdata', 'xlrd', 'xlsxwriter', 'xlwt'
 
 ForEach ($lib in $pipLibraries) {
     # Install the pip library
